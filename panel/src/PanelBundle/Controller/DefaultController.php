1b2efdd21d53c90c44dd00c2257131d2d6546af8
Omar Alejandro Valencia Macias <fmercado@mindgroup.mx>
2016-08-29 13:11:49 -0500

    Se agregaro bundle

---
+++ panel/src/PanelBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PanelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('PanelBundle:Default:index.html.twig');
+    }
+}
